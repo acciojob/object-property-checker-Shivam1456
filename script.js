5804daf6c27883c83c0cbcf66d569cc76cfe3027
Shivam1456 <127660326+Shivam1456@users.noreply.github.com>
2023-08-15 17:52:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-// const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
-// function hasKey(key) {
-//   //   write your code here
-// }
-
-// // Do not change the code below
-// const key = prompt("Enter Key.");
-// alert(hasKey(key));
-
+const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  return key in sampleObject;
+//   //   write your code here
+ return key in sampleObject;
 }
 
-// Global object definition
-let sampleObject = {
-  red: "#FF0000",
-  green: "#00FF00",
-  white: "#FFFFFF"
-};
-
-// Test the hasKey function
-console.log(hasKey("red"));    // Should return true
-console.log(hasKey("blue"));   // Should return false
-console.log(hasKey("green"));  // Should return true
-
+// Do not change the code below
+const key = prompt("Enter Key.");
 alert(hasKey(key));
+
+
+// function hasKey(key) {
+//   return key in sampleObject;
+// }
+
+// // Global object definition
+// let sampleObject = {
+//   red: "#FF0000",
+//   green: "#00FF00",
+//   white: "#FFFFFF"
+// };
+
+// // Test the hasKey function
+// console.log(hasKey("red"));    // Should return true
+// console.log(hasKey("blue"));   // Should return false
+// console.log(hasKey("green"));  // Should return true
+
+
